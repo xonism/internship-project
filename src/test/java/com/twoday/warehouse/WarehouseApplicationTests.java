d75df99e9bf6a247013924f4041f3454cb39b0d8
Gabrielė <67112338+xonism@users.noreply.github.com>
2023-08-02 11:13:10 +0300

    #1 | Create warehouse Spring Boot application (#1)
    * Add application.properties
    
    * Add pom.xml
    
    * Add StatusController
    
    * Add WarehouseApplication
    
    * Add WarehouseApplicationTests
    
    * Adjust pom.xml description
    
    * Bring back Tomcat

---
+++ src/test/java/com/twoday/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.twoday.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
