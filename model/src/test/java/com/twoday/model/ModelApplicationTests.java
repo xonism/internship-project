995bd6da11448e91bce9594567238c1872f304ac
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-03 11:14:55 +0300

    Add model module related files

---
+++ model/src/test/java/com/twoday/model/ModelApplicationTests.java
@@ -0,0 +1,12 @@
+package com.twoday.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
