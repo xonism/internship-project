e7c3e7f2eb34f60563f7b0b0a1bdc61ec56545b7
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-03 15:56:08 +0300

    Add WarehouseService to warehouse

---
+++ internship-warehouse/src/main/java/com/twoday/warehouse/services/WarehouseService.java
@@ -0,0 +1,40 @@
+package com.twoday.warehouse.services;
+
+import com.twoday.model.models.Product;
+import com.twoday.warehouse.exceptions.InsufficientQuantityException;
+import com.twoday.warehouse.exceptions.ProductNotFoundByIdException;
+import com.twoday.model.records.ProductSellRequest;
+import com.twoday.warehouse.repositories.WarehouseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WarehouseService {
+
+    private final WarehouseRepository warehouseRepository;
+
+    public WarehouseService(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+
+    public List<Product> getAll() {
+        return warehouseRepository.findAll();
+    }
+
+    public Product processSale(ProductSellRequest productSellRequest) {
+        Product product = warehouseRepository.findById(productSellRequest.id());
+
+        if (product == null) {
+            throw new ProductNotFoundByIdException(productSellRequest.id());
+        }
+
+        if (product.getQuantity() < productSellRequest.quantity()) {
+            throw new InsufficientQuantityException(productSellRequest.id());
+        }
+
+        int newQuantity = product.getQuantity() - productSellRequest.quantity();
+        product.setQuantity(newQuantity);
+        return product;
+    }
+}
