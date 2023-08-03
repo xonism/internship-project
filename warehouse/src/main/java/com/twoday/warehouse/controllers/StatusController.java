c6007f98a43130ea67f29ced2403896415592e8a
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-03 13:08:49 +0300

    Adjust StatusController RequestMapping

--- warehouse/src/main/java/com/twoday/warehouse/controllers/StatusController.java
+++ warehouse/src/main/java/com/twoday/warehouse/controllers/StatusController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/status")
+@RequestMapping("/status")
 public class StatusController {
 
     @GetMapping
