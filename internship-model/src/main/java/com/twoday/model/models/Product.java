1ebda974a185849af56518059c70fd3d0b82589c
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-03 15:54:59 +0300

    Add Product to model

---
+++ internship-model/src/main/java/com/twoday/model/models/Product.java
@@ -0,0 +1,19 @@
+package com.twoday.model.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+}
