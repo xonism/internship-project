970b5e4503e0fac935b7a136bb332c0363b00990
Gabrielė <67112338+xonism@users.noreply.github.com>
2023-08-04 08:29:56 +0300

    #3 | Add Spring Security (#5)
    * Add Spring Security to warehouse pom.xml
    
    * Add username & password to application.properties
    
    * Add WebSecurityConfiguration to warehouse
    
    * Change naming of securityFilterChain method

---
+++ warehouse/src/main/java/com/twoday/warehouse/security/WebSecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.twoday.warehouse.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username(username)
+                .password(passwordEncoder().encode(password))
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        AntPathRequestMatcher[] publicEndpoints = new AntPathRequestMatcher[]{
+                new AntPathRequestMatcher("/login"),
+                new AntPathRequestMatcher("/status")
+        };
+
+        httpSecurity.authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(publicEndpoints).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
