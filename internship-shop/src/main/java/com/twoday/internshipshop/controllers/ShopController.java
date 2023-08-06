9069abf8a305e504a88cc2eb1a3143c3eefa8907
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-06 21:48:01 +0300

    Add ShopController to shop

---
+++ internship-shop/src/main/java/com/twoday/internshipshop/controllers/ShopController.java
@@ -0,0 +1,35 @@
+package com.twoday.internshipshop.controllers;
+
+import com.twoday.internshipmodel.Product;
+import com.twoday.internshipmodel.ProductSellRequest;
+import com.twoday.internshipshop.services.WarehouseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ShopController {
+
+    private final WarehouseService warehouseService;
+
+    public ShopController(WarehouseService warehouseService) {
+        this.warehouseService = warehouseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(
+                warehouseService.getAllProductsFromWarehouse(),
+                HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> processProductSale(@RequestBody ProductSellRequest productSellRequest) {
+        return new ResponseEntity<>(
+                warehouseService.processSale(productSellRequest),
+                HttpStatus.OK);
+    }
+}
