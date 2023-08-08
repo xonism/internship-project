8953c05417b31598dfb8fb09c33619510636c1e6
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-08 09:24:34 +0300

    Move BCryptPasswordEncoder to WebSecurityConfiguration

--- internship-warehouse/src/main/java/com/twoday/internshipwarehouse/security/CustomUserDetailService.java
+++ internship-warehouse/src/main/java/com/twoday/internshipwarehouse/security/CustomUserDetailService.java
@@ -1,13 +1,10 @@
 package com.twoday.internshipwarehouse.security;
 
 import com.twoday.internshipwarehouse.services.UserService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -30,10 +27,5 @@ public class CustomUserDetailService implements UserDetailsService {
         }
 
         return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
