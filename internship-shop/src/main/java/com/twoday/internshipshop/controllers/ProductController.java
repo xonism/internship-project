0804611f5bf1701389ae0e8ec3a869671e49c01a
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-08 15:52:18 +0300

    Rename ShopController to ProductController

--- internship-shop/src/main/java/com/twoday/internshipshop/controllers/ProductController.java
+++ internship-shop/src/main/java/com/twoday/internshipshop/controllers/ProductController.java
@@ -1,21 +1,22 @@
 package com.twoday.internshipshop.controllers;
 
 import com.twoday.internshipmodel.ProductDTO;
-import com.twoday.internshipmodel.ProductSellRequest;
 import com.twoday.internshipshop.services.WarehouseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
-public class ShopController {
+public class ProductController {
 
     private final WarehouseService warehouseService;
 
-    public ShopController(WarehouseService warehouseService) {
+    public ProductController(WarehouseService warehouseService) {
         this.warehouseService = warehouseService;
     }
 
@@ -23,13 +24,6 @@ public class ShopController {
     public ResponseEntity<List<ProductDTO>> getAllProducts() {
         return new ResponseEntity<>(
                 warehouseService.getAllProducts(),
-                HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Object> processProductSale(@RequestBody ProductSellRequest productSellRequest) {
-        return new ResponseEntity<>(
-                warehouseService.processSale(productSellRequest),
                 HttpStatus.OK);
     }
 }
