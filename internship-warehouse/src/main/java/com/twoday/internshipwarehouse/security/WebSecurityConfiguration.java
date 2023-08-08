f80e14fb7d0566ea48abbcf830e1252f6b2a217a
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-08 09:19:22 +0300

    Remove CustomAuthenticationProvider related methods from WebSecurityConfiguration

--- internship-warehouse/src/main/java/com/twoday/internshipwarehouse/security/WebSecurityConfiguration.java
+++ internship-warehouse/src/main/java/com/twoday/internshipwarehouse/security/WebSecurityConfiguration.java
@@ -2,8 +2,6 @@ package com.twoday.internshipwarehouse.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,20 +13,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
-
-    private final CustomAuthenticationProvider customAuthenticationProvider;
-
-    public WebSecurityConfiguration(CustomAuthenticationProvider customAuthenticationProvider) {
-        this.customAuthenticationProvider = customAuthenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
-        return authenticationManagerBuilder.build();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
