f25be76589b60b414c46bbdf54826f611898d8dc
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-08 16:02:26 +0300

    Add @RequiredArgsConstructor

--- internship-warehouse/src/main/java/com/twoday/internshipwarehouse/services/UserService.java
+++ internship-warehouse/src/main/java/com/twoday/internshipwarehouse/services/UserService.java
@@ -2,17 +2,15 @@ package com.twoday.internshipwarehouse.services;
 
 import com.twoday.internshipwarehouse.models.User;
 import com.twoday.internshipwarehouse.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public User getByUsername(String username) {
         return userRepository.findByUsername(username)
