ceb404eaaad6e6acd33b860ecb81fa9a48ac2ed7
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-11 11:42:38 +0300

    Add REPORTS_DIR & REPORT_FILE_NAME_FORMAT to Constants

--- internship-warehouse/src/main/java/com/twoday/internshipwarehouse/constants/Constants.java
+++ internship-warehouse/src/main/java/com/twoday/internshipwarehouse/constants/Constants.java
@@ -3,6 +3,8 @@ package com.twoday.internshipwarehouse.constants;
 public class Constants {
 
     public static final String QUANTITY = "quantity";
+    public static final String REPORTS_DIR = "../reports";
+    public static final String REPORT_FILE_NAME_FORMAT = "%s/order-report-%s.csv";
 
     private Constants() {
 
