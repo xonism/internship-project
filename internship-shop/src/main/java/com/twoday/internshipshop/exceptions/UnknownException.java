efa573f091deeb44e9ae032ac49d6c15dc1a7b76
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-15 16:52:51 +0300

    Remove old shop files

--- internship-shop/src/main/java/com/twoday/internshipshop/exceptions/UnknownException.java
+++
@@ -1,8 +0,0 @@
-package com.twoday.internshipshop.exceptions;
-
-public class UnknownException extends RuntimeException {
-
-    public UnknownException(String message) {
-        super(message);
-    }
-}
