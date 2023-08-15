de871bd7a9a155df653412280830ce02dd578431
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-15 17:19:33 +0300

    Add routing

--- internship-shop/src/app/app-routing.module.ts
+++ internship-shop/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products/:id', component: ProductComponent},
+  {path: '', component: ProductListComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
