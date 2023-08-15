4a7249257d5944abca3d2c834d5244605d0bc5e8
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-15 17:17:39 +0300

    Add warehouse.service.ts

---
+++ internship-shop/src/app/services/warehouse.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../interfaces/product';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class WarehouseService {
+    private readonly productsUrl: string = `${environment.warehouseUrl}/products`;
+
+    options = {
+        headers: new HttpHeaders({
+            'Authorization': `Basic ${btoa(`${environment.username}:${environment.password}`)}`
+        })
+    };
+
+    constructor(private http: HttpClient) {
+
+    };
+
+    product$(id: string) {
+        return <Observable<Product>>this.http.get<Product>(`${this.productsUrl}/${id}`, this.options)
+            .pipe(
+                tap(console.debug),
+                catchError(this.handleError));
+    }
+
+    products$() {
+        return <Observable<Product[]>>this.http.get<Product[]>(this.productsUrl, this.options)
+            .pipe(
+                tap(console.debug),
+                catchError(this.handleError));
+    }
+
+    private handleError(error: HttpErrorResponse): Observable<never> {
+        console.error(error);
+        return throwError(() => new Error(`An error occurred - Error code: ${error.status}`));
+    };
+}
