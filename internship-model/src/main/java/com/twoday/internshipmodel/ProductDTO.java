56a972f91fbce3a952e9aa0ab5024316d119bd6c
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-16 08:23:53 +0300

    Fix formatting

--- internship-model/src/main/java/com/twoday/internshipmodel/ProductDTO.java
+++ internship-model/src/main/java/com/twoday/internshipmodel/ProductDTO.java
@@ -12,9 +12,14 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
+
     private int id;
+
     private String name;
+
     private String description;
+
     private BigDecimal price;
+
     private int quantity;
 }
