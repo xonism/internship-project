ae24ce39a8f6ea9250e94ecadecdf72402ff9bd4
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-16 08:21:57 +0300

    Change OrderCreateRequest from record to class

--- internship-model/src/main/java/com/twoday/internshipmodel/OrderCreateRequest.java
+++ internship-model/src/main/java/com/twoday/internshipmodel/OrderCreateRequest.java
@@ -1,5 +1,21 @@
 package com.twoday.internshipmodel;
 
-public record OrderCreateRequest(int productId, int quantity) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderCreateRequest {
+
+    private int productId;
+
+    private int quantity;
+
+    private BigDecimal unitPrice;
 }
