caa5807508cbbf43865bc1050b8735a2505d14c4
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-18 10:02:55 +0300

    Refactor SCSS according to new components

--- internship-shop-ui/src/app/components/product/product.component.ts
+++ internship-shop-ui/src/app/components/product/product.component.ts
@@ -1,76 +1,67 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Product } from "src/app/interfaces/product";
-import { Subscription } from 'rxjs';
-import { ShopService } from "src/app/services/shop.service";
-import { OrderCreateRequest } from "src/app/interfaces/order-create-request";
-import { Order } from "src/app/interfaces/order";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
+import {Product} from "src/app/interfaces/product";
+import {Subscription} from 'rxjs';
+import {ShopService} from "src/app/services/shop.service";
+import {OrderCreateRequest} from "src/app/interfaces/order-create-request";
+import {Order} from "src/app/interfaces/order";
 
 @Component({
-    selector: 'insh-product',
-    templateUrl: './product.component.html',
-    styleUrls: ['./product.component.scss']
+	selector: 'insh-product',
+	templateUrl: './product.component.html',
+	styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit, OnDestroy {
-    private subscriptions: Subscription[] = [];
+	private subscriptions: Subscription[] = [];
 
-    id: string = '';
-    product: Product | null = null;
-    quantity: number = 1;
-    order: Order | null = null;
+	id: string = '';
+	product: Product | null = null;
+	quantity: number = 1;
+	order: Order | null = null;
 
-    loadingMessage: string = 'Loading..'
-    isLoading: boolean = true;
+	isLoading: boolean = true;
 
-    constructor(private route: ActivatedRoute, private shopService: ShopService) {
+	constructor(private route: ActivatedRoute, private shopService: ShopService) {
 
-    }
+	}
 
-    ngOnInit() {
-        this.subscriptions.push(this.route.params.subscribe(params => {
-            this.id = params['id'];
-        }));
+	ngOnInit() {
+		this.subscriptions.push(this.route.params.subscribe(params => {
+			this.id = params['id'];
+		}));
 
-        this.getProduct();
-    }
+		this.getProduct();
+	}
 
-    decrementQuantity() {
-        if (this.quantity > 1) {
-            this.quantity -= 1;
-        }
-    }
+	submitOrder() {
+		this.isLoading = true;
 
-    incrementQuantity() {
-        if (this.product && this.quantity < this.product.quantity) {
-            this.quantity += 1;
-        }
-    }
+		const orderCreateRequest: OrderCreateRequest = {
+			productId: +this.id,
+			quantity: this.quantity,
+			unitPrice: this.product?.price
+		}
 
-    submitOrder() {
-        this.isLoading = true;
+		this.subscriptions.push(this.shopService.createOrder$(orderCreateRequest).subscribe(order => {
+			this.order = order;
+			this.getProduct();
+		}));
+	}
 
-        const orderCreateRequest: OrderCreateRequest = {
-            productId: +this.id,
-            quantity: this.quantity,
-            unitPrice: this.product?.price
-        }
+	getProduct() {
+		this.subscriptions.push(this.shopService.getProduct$(this.id).subscribe(product => {
+			this.product = product;
+			this.isLoading = false;
+		}));
+	}
 
-        this.subscriptions.push(this.shopService.createOrder$(orderCreateRequest).subscribe(order => {
-            this.order = order;
-            this.getProduct();
-        }));
-    }
+	onQuantityChanged(quantity: number) {
+		this.quantity = quantity;
+	}
 
-    getProduct() {
-        this.subscriptions.push(this.shopService.getProduct$(this.id).subscribe(product => {
-            this.product = product;
-            this.isLoading = false;
-        }));
-    }
-
-    ngOnDestroy() {
-        this.subscriptions.forEach(subscription => {
-            subscription.unsubscribe();
-        });
-    }
+	ngOnDestroy() {
+		this.subscriptions.forEach(subscription => {
+			subscription.unsubscribe();
+		});
+	}
 }
