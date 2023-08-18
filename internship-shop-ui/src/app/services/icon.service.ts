84205188739d8c37a78fcebcb231ebf284e25606
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-18 10:42:27 +0300

    Move Icons enum

--- internship-shop-ui/src/app/services/icon.service.ts
+++ internship-shop-ui/src/app/services/icon.service.ts
@@ -1,6 +1,6 @@
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
-import { Icons } from "./icons.enum";
+import { Icons } from "../enums/icons.enum";
 import { Injectable } from "@angular/core";
 
 @Injectable({
