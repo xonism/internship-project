fed23ba32101ea7ec513cd5b83e30ff97923a09c
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-18 09:56:23 +0300

    Add ProductInfoComponent

---
+++ internship-shop-ui/src/app/components/product-info/product-info.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from "@angular/core";
+import {Product} from "../../interfaces/product";
+
+@Component({
+	selector: 'insh-product-info',
+	templateUrl: './product-info.component.html',
+	styleUrls: ['./product-info.component.scss']
+})
+export class ProductInfoComponent {
+
+	@Input()
+	product: Product | null = null;
+
+	loadingMessage: string = 'Loading..'
+
+	renderPrice() {
+		return !this.product
+			? this.loadingMessage
+			: `${this.product.price} €`;
+	}
+}
