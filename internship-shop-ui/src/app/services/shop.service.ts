c162423705da8f8ba5261ff8bfc5079b9baf4f1d
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-21 08:48:13 +0300

    Remove handleError from shop.service.ts

--- internship-shop-ui/src/app/services/shop.service.ts
+++ internship-shop-ui/src/app/services/shop.service.ts
@@ -1,8 +1,8 @@
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Product} from '../interfaces/product';
-import {Observable, throwError} from 'rxjs';
-import {catchError, tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 import {environment} from 'src/environments/environment';
 import {OrderCreateRequest} from '../interfaces/order-create-request';
 import {Order} from '../interfaces/order';
@@ -18,27 +18,16 @@ export class ShopService {
 
 	getProduct$(id: string) {
 		return <Observable<Product>>this.http.get<Product>(`${this.productsUrl}/${id}`)
-			.pipe(
-				tap(console.debug),
-				catchError(this.handleError));
+			.pipe(tap(console.debug));
 	}
 
 	getProducts$() {
 		return <Observable<Product[]>>this.http.get<Product[]>(this.productsUrl)
-			.pipe(
-				tap(console.debug),
-				catchError(this.handleError));
+			.pipe(tap(console.debug));
 	}
 
 	createOrder$(orderCreateRequest: OrderCreateRequest) {
 		return <Observable<Order>>this.http.post<Order>(this.ordersUrl, orderCreateRequest)
-			.pipe(
-				tap(console.debug),
-				catchError(this.handleError));
+			.pipe(tap(console.debug));
 	}
-
-	private handleError(error: HttpErrorResponse): Observable<never> {
-		console.error(error);
-		return throwError(() => new Error(`An error occurred - Error code: ${error.status}`));
-	};
 }
