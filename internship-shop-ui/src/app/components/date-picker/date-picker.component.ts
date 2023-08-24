9a7f9119fc94652fe03977418256adf952af00e5
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-24 15:07:28 +0300

    Add DatePickerComponent

---
+++ internship-shop-ui/src/app/components/date-picker/date-picker.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+
+@Component({
+	selector: 'app-date-picker',
+	templateUrl: './date-picker.component.html',
+	styleUrls: ['./date-picker.component.scss']
+})
+export class DatePickerComponent {
+
+	@Input()
+	date!: Date;
+
+	@Input()
+	maxDate!: Date;
+
+	@Output()
+	onDateChanged: EventEmitter<Date> = new EventEmitter<Date>();
+
+	emitChange(): void {
+		this.onDateChanged.emit(this.date);
+	}
+}
