5bd00c8c70cd3e7c1d5d86773552e8ee4f3d9103
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-24 15:30:11 +0300

    Add ReportComponent

--- internship-shop-ui/src/app/services/shop.service.ts
+++ internship-shop-ui/src/app/services/shop.service.ts
@@ -1,8 +1,7 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {IProduct} from '../interfaces/product';
 import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
 import {environment} from 'src/environments/environment';
 import {IOrderCreateRequest} from '../interfaces/order-create-request';
 import {IOrder} from '../interfaces/order';
@@ -12,20 +11,35 @@ export class ShopService {
 
 	private readonly productsUrl: string = `${environment.shopUrl}/products`;
 	private readonly ordersUrl: string = `${environment.shopUrl}/orders`;
+	private readonly reportsUrl: string = `${this.ordersUrl}/reports`;
 
 	constructor(private http: HttpClient) {
 
 	};
 
-	getProduct$(id: string) {
+	getReportData$(dateTime: string): Observable<string> {
+		return <Observable<string>>this.http.get(
+			`${this.reportsUrl}/${dateTime}`,
+			{responseType: 'text'}
+		);
+	}
+
+	getReportDataBlob$(dateTime: string): Observable<HttpResponse<Blob>> {
+		return <Observable<HttpResponse<Blob>>>this.http.get(
+			`${this.reportsUrl}/${dateTime}`,
+			{responseType: 'blob', observe: 'response'}
+		);
+	}
+
+	getProduct$(id: string): Observable<IProduct> {
 		return <Observable<IProduct>>this.http.get<IProduct>(`${this.productsUrl}/${id}`);
 	}
 
-	getProducts$() {
+	getProducts$(): Observable<IProduct[]> {
 		return <Observable<IProduct[]>>this.http.get<IProduct[]>(this.productsUrl);
 	}
 
-	createOrder$(orderCreateRequest: IOrderCreateRequest) {
+	createOrder$(orderCreateRequest: IOrderCreateRequest): Observable<IOrder> {
 		return <Observable<IOrder>>this.http.post<IOrder>(this.ordersUrl, orderCreateRequest);
 	}
 }
