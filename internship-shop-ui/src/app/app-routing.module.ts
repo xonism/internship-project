376b56144d4213fbe4200bfe762e929a87977680
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-25 16:29:43 +0300

    Add statistics route

--- internship-shop-ui/src/app/app-routing.module.ts
+++ internship-shop-ui/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from './components/product-list/product-list.component';
 import {ProductComponent} from './components/product/product.component';
 import {ReportComponent} from "./components/report/report.component";
+import {StatisticsComponent} from "./components/statistics/statistics.component";
 
 const routes: Routes = [
 	{path: 'product/:id', component: ProductComponent},
 	{path: 'reports', component: ReportComponent},
+	{path: 'statistics', component: StatisticsComponent},
 	{path: 'products', component: ProductListComponent, pathMatch: 'full'},
 	{path: '**', redirectTo: '/products', pathMatch: 'full'}
 ];
