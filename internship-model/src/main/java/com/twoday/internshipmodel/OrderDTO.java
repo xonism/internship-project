9556e244aa8329f0b700444d7f332f10b4717474
Gabrielė Mincytė <gabriele.minc@protonmail.com>
2023-08-25 16:35:51 +0300

    Add timestamp to OrderDTO

--- internship-model/src/main/java/com/twoday/internshipmodel/OrderDTO.java
+++ internship-model/src/main/java/com/twoday/internshipmodel/OrderDTO.java
@@ -1,7 +1,8 @@
 package com.twoday.internshipmodel;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
-public record OrderDTO(int id, int userId, int productId, int quantity, BigDecimal unitPrice) {
+public record OrderDTO(int id, int userId, int productId, int quantity, BigDecimal unitPrice, LocalDateTime timestamp) {
 
 }
